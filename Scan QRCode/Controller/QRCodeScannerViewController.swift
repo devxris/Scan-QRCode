e24407abfb85ffb85ac93002ce509ac4c5a3bd42
devxris <chuwei0915@gmail.com>
2017-09-26 22:44:34 +0800

    Add barcode scanner

--- Scan QRCode/Controller/QRCodeScannerViewController.swift
+++ Scan QRCode/Controller/QRCodeScannerViewController.swift
@@ -20,6 +20,16 @@ class QRCodeScannerViewController: UIViewController {
 	var videoPreviewLayer: AVCaptureVideoPreviewLayer?
 	var qrcodeFrameView: UIView?
 	
+	// Added to support different barcodes
+	let supportedBarCodes = [AVMetadataObject.ObjectType.qr,
+	                         AVMetadataObject.ObjectType.code128,
+	                         AVMetadataObject.ObjectType.code39,
+	                         AVMetadataObject.ObjectType.code93,
+	                         AVMetadataObject.ObjectType.upce,
+	                         AVMetadataObject.ObjectType.pdf417,
+	                         AVMetadataObject.ObjectType.ean13,
+	                         AVMetadataObject.ObjectType.aztec ]
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		
@@ -40,7 +50,10 @@ class QRCodeScannerViewController: UIViewController {
 			
 			// Set delegate and use the default dispatch queue to execute the call back
 			captureMetadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
-			captureMetadataOutput.metadataObjectTypes = [AVMetadataObject.ObjectType.qr]
+			// captureMetadataOutput.metadataObjectTypes = [AVMetadataObject.ObjectType.qr]
+			// Detect all the supported bar code
+			captureMetadataOutput.metadataObjectTypes = supportedBarCodes
+
 			
 			// Initialize the video preview layer and add it as a sublayer to the viewPreview view's layer
 			videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession!)
@@ -74,16 +87,21 @@ extension QRCodeScannerViewController: AVCaptureMetadataOutputObjectsDelegate {
 		// Check if the metadataObjects array is not nil and it contains at least one object
 		if metadataObjects.count == 0 {
 			qrcodeFrameView?.frame = CGRect.zero
-			messageLabel.text = "No QR code is detected"
+			messageLabel.text = "No barcode/QR code is detected"
 			return
 		}
 		
 		// Get the metadata object
 		let metadataObj = metadataObjects[0] as! AVMetadataMachineReadableCodeObject
-		if metadataObj.type == AVMetadataObject.ObjectType.qr {
-			// If the found metadata is equal to the QR code metadata, update status label's text and set the bounds
+		// Here we use filter method to check if the type of metadataObj is supported
+		// Instead of hardcoding the AVMetadataObjectTypeQRCode, we check if the type
+		// can be found in the array of supported bar codes.
+		if supportedBarCodes.contains(metadataObj.type) {
+			//        if metadataObj.type == AVMetadataObjectTypeQRCode {
+			// If the found metadata is equal to the QR code metadata then update the status label's text and set the bounds
 			let barCodeObject = videoPreviewLayer?.transformedMetadataObject(for: metadataObj)
 			qrcodeFrameView?.frame = barCodeObject!.bounds
+			
 			if metadataObj.stringValue != nil {
 				messageLabel.text = metadataObj.stringValue
 			}
