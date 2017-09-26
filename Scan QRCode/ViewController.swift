ca36e31ec103237ea3785f522257b6525fb05f98
devxris <chuwei0915@gmail.com>
2017-09-26 17:11:56 +0800

    Initial Commit

---
+++ Scan QRCode/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Scan QRCode
+//
+//  Created by Chris Huang on 26/09/2017.
+//  Copyright © 2017 Chris Huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
